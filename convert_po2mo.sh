e4004f542ede8b459e61e000db02f35252d3aee5
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-07-05 11:07:07 +0200

    Remove \n

--- convert_po2mo.sh
+++ convert_po2mo.sh
@@ -5,5 +5,6 @@ for lang in */; do
   [ -L "${lang%/}" ] && continue
   echo "[ * ] Update $lang "
   mkdir -p "../build/${lang%/}/LC_MESSAGES/"
+  sed -i "s/\\n/\n/g" "./${lang%/}/LC_MESSAGES/hestiacp.po"
   msgfmt "./${lang%/}/LC_MESSAGES/hestiacp.po" -o "../build/${lang%/}/LC_MESSAGES/hestiacp.mo"
 done
